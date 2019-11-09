bc0d513364e39f5424c5dc782be704df3078911c
Muhammad Zuhaily <2019705303@isiswa.uitm.edu.my>
2019-11-09 14:25:49 +0800

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'zuhaily',
+            'email' => '2019705303@isiswa.uitm.edu.my',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'muhammad',
+            'email' => '2016801896@isiswa.uitm.edu.my',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
